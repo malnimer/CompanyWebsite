a7b50ab79abcaa8462799053a91027d170826409
malnimer <malnimer@cps.edu>
2016-04-25 14:24:31 +0000

    first commit

---
+++ app/controllers/websites_controller.rb
@@ -0,0 +1,27 @@
+class WebsitesController < ApplicationController
+   
+   def activities 
+       render "activities"
+   end
+   
+    def about_us 
+       render "about-us"
+    end
+   
+    def contact_us 
+       render "contact_us"
+    end
+   
+    def home 
+       render "home"
+    end
+   
+    def registration 
+       render "registration"
+    end
+   
+    def schedule 
+       render "schedule"
+    end
+   
+end
